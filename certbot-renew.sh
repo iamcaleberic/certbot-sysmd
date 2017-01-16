e1155397408cb9fab37c9cf111bdcffd69653c19
Eric Caleb <calebme@mail.com>
2017-01-16 17:46:57 +0000

    Modified to add select menu

--- certbot-renew.sh
+++ certbot-renew.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
-LE=/opt/letsencrypt
-cd $LE && ./certbot-auto renew --dry-run
 
+LE=/opt/letsencrypt
+
+function renew {
+   cd $LE && ./certbot-auto renew 
+}
+
+function test {
+  cd $LE && ./certbot-auto renew --dry-run
+}
+
+OPTIONS="Test Renew"
+select opt in $OPTIONS; do
+   if [ "$opt" = "Test" ]; then
+	  echo Running dry run...
+	  test	
+	  exit
+
+  elif [ "$opt" = "Renew" ]; then
+	  echo Renewing
+	  renew
+	  echo Renewal complete
+	  exit
+	  					
+  else	
+	 clear
+	 echo bad option
+  fi
+
+done
+																									                 
