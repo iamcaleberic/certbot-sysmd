cbe405e9e19d81bfb72703c9dc371bfa05fed558
Eric Caleb <calebme@mail.com>
2017-07-19 13:50:46 +0100

    add bash script to run apache renewal and dry run

---
+++ certbot-apache.sh
@@ -0,0 +1,44 @@
+#bin/bash
+
+function renew {
+   certbot renew --apache
+   if [ $? -eq 0 ]
+   then
+     echo "Certificate Renewal Complete :)"
+   else
+     echo "Error while attempting renewal :("
+   fi
+}
+
+function test {
+  certbot renew --apache --dry-run && sudo service apache2 restart
+  if [ $? -eq 0 ]
+  then
+   echo "Dry Run Complete :)"
+  else
+    echo "Error while running dry run :("
+  fi
+}
+
+OPTIONS="Test Renew Quit"
+select opt in $OPTIONS; do
+   if [ "$opt" = "Test" ]; then
+	  echo Running dry run...
+	  test
+    echo Dry Run complete.
+	  exit
+
+  elif [ "$opt" = "Renew" ]; then
+	  echo Renewing...
+	  renew
+	  echo Renewal complete.
+	  exit
+
+  elif [ "$opt" = "Quit" ]; then
+	  exit
+  else
+	 clear
+	 echo Bad option
+  fi
+
+done
