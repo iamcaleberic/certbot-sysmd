b0d9a6923303751c9bb5f22374c9e5722d2db190
Eric Caleb <calebme@mail.com>
2017-07-19 13:51:17 +0100

    add script to run dry run and renewal for nginx

---
+++ certbot-nginx.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+function renew {
+   certbot renew --nginx
+   if [ $? -eq 0 ]
+   then
+     echo "Certificate Renewal Complete :)"
+   else
+     echo "Error while attempting renewal :("
+   fi
+}
+
+function test {
+  certbot renew --nginx --dry-run && sudo service nginx restart
+  if [ $? -eq 0 ]
+  then
+   echo "Dry Run Complete :)"
+  else
+    echo "Error while running dry run :("
+  fi
+}
+
+OPTIONS="Test Renew Quit"
+select opt in $OPTIONS; do
+   if [ "$opt" = "Test" ]; then
+	  echo Running dry run...
+	  test
+    echo Dry Run complete.
+	  exit
+
+  elif [ "$opt" = "Renew" ]; then
+	  echo Renewing...
+	  renew
+	  echo Renewal complete.
+	  exit
+
+  elif [ "$opt" = "Quit" ]; then
+	  exit
+  else
+	 clear
+	 echo Bad option
+  fi
+
+done
